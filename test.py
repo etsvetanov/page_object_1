48dd9278c54820a1e222f80d6655c80937539230
Evgeni Tsvetanov <etsvet@softserveinc.com>
2016-09-28 13:20:46 +0300

    Switching back to Chrome()

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 from unittest import TestCase
 from selenium import webdriver
-
+# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#
+# caps = DesiredCapabilities.FIREFOX
+#
+# caps['marionette'] = True
+# caps['binary'] = '/Applications/Firefox.app/Contents/MacOS/firefox-bin'
 
 class BaseTestCase(TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
+        # self.driver = webdriver.Chrome('~/Documents/WebDriver/chromedriver')
         self.driver.implicitly_wait(30)
         self.driver.maximize_window()
         self.driver.get('http://www.python.org/')
